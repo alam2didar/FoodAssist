430902290fb386847b394c2a6560ecc2847f705a
Yu Qiao <yu.umlaut.qiao@outlook.com>
2022-06-16 16:58:28 +0200

    set evaluation_finished at success, catch ValueError exception

--- worker_evaluator.py
+++ worker_evaluator.py
@@ -18,41 +18,46 @@ class WorkerEvaluator(QObject):
             if archive_file_name is None:
                 print("archive_file_name is None")
             else:
-                print("archive_file_name is: ", archive_file_name)
-                # load data to process
-                column_names = ['timestamp', 'step', 'sensor_type', 'result_feature']
-                df = pd.read_csv(self.archive_file_name, header=None, names=column_names)
-                df = df.dropna()
-                df_position = df[df['sensor_type'] == 'position']
-                df_motion = df[df['sensor_type'] == 'motion']
-                # creating image, label_new_plot_3
-                new_plot_1 = sns.countplot(x = 'step',
-                        hue = 'result_feature',
-                        data = df_position,
-                        order = df_position.step.value_counts().index);
-                self.fig_1_name = "myfig_1.png"
-                new_plot_1.figure.savefig(self.fig_1_name)
-                # creating image, label_new_plot_3
-                new_plot_2 = sns.countplot(x = 'step',
-                        hue = 'result_feature',
-                        data = df_motion,
-                        order = df_motion.step.value_counts().index);
-                self.fig_2_name = "myfig_2.png"
-                new_plot_2.figure.savefig(self.fig_2_name)
-                # process for result text, label_new_plot_3
-                df_position_step_1 = df_position[df_position['step'] == 'step_1']
-                df_position_step_1_feature_1 = df_position_step_1[df_position_step_1['result_feature'] == 1]
-                df_position_step_1_feature_2 = df_position_step_1[df_position_step_1['result_feature'] == 2]
-                df_position_step_1_feature_3 = df_position_step_1[df_position_step_1['result_feature'] == 3]
-                sum = df_position_step_1_feature_1.shape[0] + df_position_step_1_feature_2.shape[0] + df_position_step_1_feature_3.shape[0]
-                result_ratio = df_position_step_1_feature_1.shape[0] / sum
-                print("result_ratio:", result_ratio)
-                # to do - define expert range?
-                if result_ratio >= 0.6 and result_ratio <= 1:
-                    self.result_text = "In step 1, you have been scoring the chopping gesture (picture 1) more frequently than the expert."
-                elif result_ratio > 0.4 and result_ratio < 0.6:
-                    self.result_text = "In step 1, you have been scoring the vertical gesture (picture 2) more frequently than the expert."
-                elif result_ratio >= 0 and result_ratio <= 0.4:
-                    self.result_text = "In step 1, you have been scoring the peeling gesture (picture 3) more frequently than the expert."
-                # emit result
-                self.evaluation_finished.emit(self.fig_1_name, self.fig_2_name, self.result_text)
+                try:
+                    print("archive_file_name is: ", archive_file_name)
+                    # load data to process
+                    column_names = ['timestamp', 'step', 'sensor_type', 'result_feature']
+                    df = pd.read_csv(archive_file_name, header=None, names=column_names)
+                    df = df.dropna()
+                    df_position = df[df['sensor_type'] == 'position']
+                    df_motion = df[df['sensor_type'] == 'motion']
+                    # creating image, label_new_plot_3
+                    new_plot_1 = sns.countplot(x = 'step',
+                            hue = 'result_feature',
+                            data = df_position,
+                            order = df_position.step.value_counts().index);
+                    self.fig_1_name = "myfig_1.png"
+                    new_plot_1.figure.savefig(self.fig_1_name)
+                    # creating image, label_new_plot_3
+                    new_plot_2 = sns.countplot(x = 'step',
+                            hue = 'result_feature',
+                            data = df_motion,
+                            order = df_motion.step.value_counts().index);
+                    self.fig_2_name = "myfig_2.png"
+                    new_plot_2.figure.savefig(self.fig_2_name)
+                    # process for result text, label_new_plot_3
+                    df_position_step_1 = df_position[df_position['step'] == 'step_1']
+                    df_position_step_1_feature_1 = df_position_step_1[df_position_step_1['result_feature'] == 1]
+                    df_position_step_1_feature_2 = df_position_step_1[df_position_step_1['result_feature'] == 2]
+                    df_position_step_1_feature_3 = df_position_step_1[df_position_step_1['result_feature'] == 3]
+                    sum = df_position_step_1_feature_1.shape[0] + df_position_step_1_feature_2.shape[0] + df_position_step_1_feature_3.shape[0]
+                    result_ratio = df_position_step_1_feature_1.shape[0] / sum
+                    print("result_ratio:", result_ratio)
+                    # to do - define expert range?
+                    if result_ratio >= 0.6 and result_ratio <= 1:
+                        self.result_text = "In step 1, you have been scoring the chopping gesture (picture 1) more frequently than the expert."
+                    elif result_ratio > 0.4 and result_ratio < 0.6:
+                        self.result_text = "In step 1, you have been scoring the chopping gesture (picture 1) as frequently as the expert."
+                    elif result_ratio >= 0 and result_ratio <= 0.4:
+                        self.result_text = "In step 1, you have been scoring the chopping gesture (picture 1) less frequently than the expert."
+                    # set evaluation finished flag
+                    self.evaluation_finished = True
+                    print("reaching point - evaluation finished")
+                except ValueError:
+                    print(ValueError)
+                    print("reaching point - error encountered")
