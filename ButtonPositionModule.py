c63bee3737fe2654db0b5b87db061d08da560f75
Yu Qiao <>
2022-06-21 17:26:15 +0200

    update coordinates

--- ButtonPositionModule.py
+++ ButtonPositionModule.py
@@ -3,13 +3,13 @@ class ButtonPositioner:
         # add attributes
         self.last_button_clicked = None
         self.counter = 0
-        self.left = type('left', (ButtonPositioner,), {'left': 410, 'right': 470, 'top': 400, 'bottom': 455, 'short': 1200, 'long': 1400})
-        self.right = type('right', (ButtonPositioner,), {'left': 480, 'right': 560, 'top': 350, 'bottom': 430, 'short': 1200, 'long': 1400})
-        self.step_1 = type('step_1', (ButtonPositioner,), {'left': 215, 'right': 255, 'top': 395, 'bottom': 455, 'short': 1200, 'long': 1400})
-        self.step_2 = type('step_2', (ButtonPositioner,), {'left': 280, 'right': 320, 'top': 395, 'bottom': 455, 'short': 1200, 'long': 1400})
-        self.step_3 = type('step_3', (ButtonPositioner,), {'left': 340, 'right': 370, 'top': 395, 'bottom': 455, 'short': 1200, 'long': 1400})
-        self.step_4 = type('step_4', (ButtonPositioner,), {'left': 385, 'right': 425, 'top': 395, 'bottom': 455, 'short': 1200, 'long': 1400})
-        self.restart = type('restart', (ButtonPositioner,), {'left': 320, 'right': 350, 'top': 320, 'bottom': 460, 'short': 1200, 'long': 1400})
+        self.left = type('left', (ButtonPositioner,), {'left': 1130, 'right': 1208, 'top': 725, 'bottom': 833, 'short': 1200, 'long': 1400})
+        self.right = type('right', (ButtonPositioner,), {'left': 1288, 'right': 1404, 'top': 725, 'bottom': 833, 'short': 1200, 'long': 1400})
+        self.step_1 = type('step_1', (ButtonPositioner,), {'left': 672, 'right': 757, 'top': 407, 'bottom': 499, 'short': 1200, 'long': 1400})
+        self.step_2 = type('step_2', (ButtonPositioner,), {'left': 807, 'right': 887, 'top': 407, 'bottom': 499, 'short': 1200, 'long': 1400})
+        self.step_3 = type('step_3', (ButtonPositioner,), {'left': 924, 'right': 1007, 'top': 407, 'bottom': 499, 'short': 1200, 'long': 1400})
+        self.step_4 = type('step_4', (ButtonPositioner,), {'left': 1064, 'right': 1137, 'top': 407, 'bottom': 499, 'short': 1200, 'long': 1400})
+        self.restart = type('restart', (ButtonPositioner,), {'left': 870, 'right': 945, 'top': 548, 'bottom': 620, 'short': 1200, 'long': 1400})
 
     def update_last_button_and_counter(self, type):
         # if last button clicked is this button, then increase counter
@@ -20,8 +20,8 @@ class ButtonPositioner:
             self.counter = 0
         # update last button clicked
         self.last_button_clicked = type
-        # return true only after accumulated 20 times
-        if self.counter>=20:
+        # return true only after accumulated 5 times
+        if self.counter >= 5:
             return True
 
     def check_in_area(self, x, y, z, button_type):
