124ea2811d16d988bb794f0ff033861c4f81fd9f
Didar Alam <>
2022-07-30 19:50:30 +0200

    bugfix: reset button counter

--- ButtonPositionModule.py
+++ ButtonPositionModule.py
@@ -29,8 +29,9 @@ class ButtonPositioner:
             self.counter = 0
         # update last button clicked
         self.last_button_clicked = button_type
-        # return true only after accumulated 10 times
-        if self.counter >= 5 and button_type is not self.v_cont:
+        # return true only after accumulated 5 times
+        if self.counter > 4:
+            self.counter = 0
             return True
 
     def check_in_area(self, x, y, z, button_type):
