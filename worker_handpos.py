00d6feb35ebed2513c1f0e63d30af508bdfc051b
LabComputer <>
2022-11-10 13:59:49 +0100

    use INDEX_FINGER_MCP for cursor

--- worker_handpos.py
+++ worker_handpos.py
@@ -30,7 +30,7 @@ class WorkerHandPos(QObject):
     def get_hand_position(self, use_mediapipe=True, use_depth_contour=False): # A slot takes no params
         counter = 0
         # while loop
-        while self.worker_activated:
+        while True:
             counter = counter + 1
 # debug no camera
             if self.depth_camera:
@@ -38,9 +38,9 @@ class WorkerHandPos(QObject):
                 ret, depth_image, depth_colormap, color_image, bg_removed = self.depth_camera.getFrame()
                 # alternative 1 - find point based on mediapipe
                 color_image_to_process, results = self.hand_detector.findHands(color_image)
-                # find specified knuckle coordinates INDEX_FINGER_TIP
+                # find specified knuckle coordinates INDEX_FINGER_MCP
                 if use_mediapipe:
-                    point = self.hand_detector.findPosition(color_image_to_process, results, targetId=8)
+                    point = self.hand_detector.findPosition(color_image_to_process, results, targetId=5)
                 # alternative 2 - find point based on depth contour
                 # if not point and use_depth_contour:
                 #     point = self.depth_contour_finder.findPosition(depth_colormap)
